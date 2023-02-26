090122b775c0cc9a93de79c80bdbd556115a8790
WhoIsYgim <e.larkin3004@gmail.com>
2023-02-27 01:47:47 +0300

    CM-1.2: изменить порядок инъекции зависимостей

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,13 +1,23 @@
 package app
 
 import (
-	"depeche/internal/auth/delivery/http"
+	"depeche/internal/delivery/handlers"
+	"depeche/internal/delivery/middleware"
+	storage "depeche/internal/repository/localStorage"
 	httpserver "depeche/internal/server"
+	session "depeche/internal/session/localStorage"
+	"depeche/internal/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
-	router := initRouter()
+	data := storage.NewMemoryStorage()
+	sessionStorage := session.NewMemorySessionStorage()
+	userService := usecase.NewUserService(data, sessionStorage)
+	userHandler := handlers.NewUserHandler(userService)
+	authMiddleware := middleware.NewAuthMiddleware(userService)
+	router := initRouter(userHandler, authMiddleware)
 	server := httpserver.NewServer(router)
 
 	err := server.ListenAndServe()
@@ -16,22 +26,22 @@ func Run() {
 	}
 }
 
-func initRouter() *gin.Engine {
+func initRouter(userHandler *handlers.UserHandler, authMW *middleware.AuthMiddleware) *gin.Engine {
 	router := gin.Default()
 
-	authHandler := http.NewAuthHandler()
-
 	// вешаем авторизационную миддлвару на все api
-	authMiddleware := http.NewAuthMiddleware(authHandler.Service)
+	apiEndpointsGroup := router.Group("/api", authMW.Middleware())
 
-	apiEndpointsGroup := router.Group("/api", authMiddleware)
 	// тестовый эндпоинт
 	apiEndpointsGroup.GET("/test", func(context *gin.Context) {
 		context.Writer.WriteString("TEST")
 	})
 
-	// регистриурем урлы авторизации и регистрации
-	http.BindAuthEndpoints(router, authHandler)
+	authEndpointsGroup := router.Group("/auth")
+	{
+		authEndpointsGroup.POST("/sign-in", userHandler.SignIn)
+		authEndpointsGroup.POST("/sign-up", userHandler.SignUp)
+	}
 
 	return router
 }
