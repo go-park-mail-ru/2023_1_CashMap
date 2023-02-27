7c831e7020f11e6751e236571356eef88f84de20
WhoIsYgim <e.larkin3004@gmail.com>
2023-02-27 17:40:11 +0300

    CM-1.2: добавить метод isExpired для сессии

--- internal/session/session.go
+++ internal/session/session.go
@@ -1,8 +1,17 @@
 package session
 
-import "time"
+import (
+	"time"
+)
 
 type Session struct {
 	Email     string
 	ExpiresAt time.Time
+}
+
+func (s *Session) isExpired() bool {
+	if time.Now().Before(s.ExpiresAt) {
+		return true
+	}
+	return false
 }
