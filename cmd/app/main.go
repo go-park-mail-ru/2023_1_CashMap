b5daebc79b191f3bbef548a68d7d85c60cdb73fa
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2023-03-02 21:53:57 +0300

    CM-1.14: Добавить аннотации к структурам, эндпоинтам и main для генерации swagger документации, добавить файлы документации

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "depeche/internal/app"
+import (
+	"depeche/internal/app"
+)
+
+//	@title			Depeche API
+//	@version		1.0
+//	@description	Api for Depeche social network. VK Education project. Spring, 2023
+
+//	@contact.name	CashMap team
+
+//	@host		localhost:8080
+//	@BasePath	/api
 
 func main() {
 	app.Run()
