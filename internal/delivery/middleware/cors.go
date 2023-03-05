ad922c5aad24b241009380af0b976157d1381eb6
WhoIsYgim <e.larkin3004@gmail.com>
2023-03-05 16:34:50 +0300

    CM-1.12: Настройка CORS

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -9,8 +9,8 @@ func CORS() gin.HandlerFunc {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowCredentials = true
 	corsConfig.AllowMethods = []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"}
-	corsConfig.AllowHeaders = []string{"Content-Type"}
-	corsConfig.AllowOrigins = []string{"http://95.163.212.121:8000"}
+	corsConfig.AllowHeaders = []string{"Content-Type", "Origin"}
+	corsConfig.AllowOrigins = []string{"http://95.163.212.121:8080"}
 
 	return cors.New(corsConfig)
 }
