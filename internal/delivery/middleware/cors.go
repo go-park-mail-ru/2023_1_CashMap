a3e4a4619b82937698d4c3996396d4cfecbe43d0
WhoIsYgim <e.larkin3004@gmail.com>
2023-03-05 17:44:48 +0300

    CORS

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -10,7 +10,7 @@ func CORS() gin.HandlerFunc {
 	corsConfig.AllowCredentials = true
 	corsConfig.AllowMethods = []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"}
 	corsConfig.AllowHeaders = []string{"Content-Type", "Origin"}
-	corsConfig.AllowOrigins = []string{"http://95.163.212.121:8080"}
+	corsConfig.AllowOrigins = []string{"http://95.163.212.121:8000", "http://95.163.212.121:8080", "http://95.163.212.121:80"}
 
 	return cors.New(corsConfig)
 }
