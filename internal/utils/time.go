9aef898545b08bccf4df2dd9fddedc72e1fc7a4e
thxtomslf <purepvll@gmail.com>
2023-04-08 18:34:15 +0300

    CM-2.18.1: Изменить формат времени

--- internal/utils/time.go
+++ internal/utils/time.go
@@ -1,15 +1,10 @@
 package utils
 
 import (
-	"fmt"
 	"time"
 )
 
-var format = "%d-%02d-%02dT%02d:%02d:%02d"
-
 func CurrentTimeString() string {
-	t := time.Now()
-	return fmt.Sprint(format,
-		t.Year(), t.Month(), t.Day(),
-		t.Hour(), t.Minute(), t.Second())
+	currentTime := time.Now().String()
+	return currentTime
 }
