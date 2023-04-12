7a01086e49f1a164383b92c37631f55e737a7442
thxtomslf <purepvll@gmail.com>
2023-04-12 16:01:39 +0300

    CM-2.20: Фикс корса

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -9,9 +9,10 @@ func CORS() gin.HandlerFunc {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowCredentials = true
 	corsConfig.AllowMethods = []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"}
-	corsConfig.AllowHeaders = []string{"Content-Type", "Origin", "X-Csrf-Token"}
+	corsConfig.AllowHeaders = []string{"Content-Type", "Origin", "X-Csrf-Token", "Access-Control-Expose-Headers"}
 	corsConfig.AllowOrigins = []string{"http://95.163.212.121:8000", "http://95.163.212.121:8080", "http://95.163.212.121:80",
 		"http://127.0.0.1:8000", "http://127.0.0.1:8080"}
+	corsConfig.ExposeHeaders = []string{"Access-Control-Expose-Headers"}
 
 	return cors.New(corsConfig)
 }
