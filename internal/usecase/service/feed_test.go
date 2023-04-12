3a4948e25ecdaf2c2028583e8de7dca9220a7e42
thxtomslf <purepvll@gmail.com>
2023-04-12 12:38:27 +0300

    CM-2.20: Мердж тестов, XSS и CSRF

---
+++ internal/usecase/service/feed_test.go
@@ -0,0 +1,16 @@
+package service
+
+import "testing"
+
+func Test(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+		})
+	}
+}
