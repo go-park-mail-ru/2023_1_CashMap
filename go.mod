cdbb6042bf409afb9263beedbdcf7bf438343657
Egor Larkin <e.larkin3004@gmail.com>
2023-04-21 14:19:25 +0300

    CM-3.1-Добавить логгер для error middleware

--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 )
--- internal/delivery/middleware/error.go
+++ internal/delivery/middleware/error.go
@@ -2,25 +2,26 @@ package middleware
 
 import (
 	"depeche/pkg/apperror"
+	"depeche/pkg/logs"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ErrorMiddleware() gin.HandlerFunc {
+	logger := logs.GetLogger()
 	return func(ctx *gin.Context) {
 		ctx.Next()
 
 		if len(ctx.Errors) == 0 {
 			return
 		}
-
 		err := ctx.Errors[0].Unwrap()
 
 		serverError, ok := err.(*apperror.ServerError)
 		if ok {
 			err = serverError.UserErr
 		}
-
+		logger.Error(serverError.Unwrap())
 		ctx.JSON(Errors[err].Code, gin.H{
 			"status":  Errors[err].Code,
 			"message": Errors[err].Message,
