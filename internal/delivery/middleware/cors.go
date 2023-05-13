9717a845cefab828e607523e3c7d64b045947cc1
Egor Larkin <e.larkin3004@gmail.com>
2023-05-13 18:07:31 +0300

    CORS upd

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -9,9 +9,13 @@ func CORS() gin.HandlerFunc {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowCredentials = true
 	corsConfig.AllowMethods = []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"}
-	corsConfig.AllowHeaders = []string{"Content-Type", "Origin", "X-Csrf-Token", "Access-Control-Expose-Headers"}
-	corsConfig.AllowOrigins = []string{"http://95.163.212.121:8000", "http://95.163.212.121:8080", "http://95.163.212.121:80",
-		"http://127.0.0.1:8000", "http://127.0.0.1:8080"}
+	corsConfig.AllowHeaders = []string{"Content-Type", "Origin", "X-Csrf-Token",
+		"Access-Control-Expose-Headers", "Connection"}
+	corsConfig.AllowOrigins = []string{
+		"http://95.163.212.121:8000", "http://95.163.212.121:8080",
+		"http://95.163.212.121:80",
+		"http://127.0.0.1:8000", "http://127.0.0.1:8080",
+		"https://depeche.su"}
 	corsConfig.ExposeHeaders = []string{"X-Csrf-Token"}
 
 	return cors.New(corsConfig)
